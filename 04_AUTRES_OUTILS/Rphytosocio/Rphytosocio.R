5006345a10839714dc4f44641a3bdb48fc52011e
Augustin Soulard <42175782+augustinsoulard@users.noreply.github.com>
2023-09-15 16:17:20 +0200

    v0.3.2

--- 04_AUTRES_OUTILS/Rphytosocio/Rphytosocio.R
+++ 04_AUTRES_OUTILS/Rphytosocio/Rphytosocio.R
@@ -5,15 +5,16 @@ setwd(WD)
 # Chargement des packages
 if(!require("foreign")){install.packages("foreign")} ; library("foreign")
 if(!require("reshape2")){install.packages("reshape2")} ; library("reshape2")
+if(!require("tidyverse")){install.packages("tidyverse")} ; library("tidyverse")
 
 # Charger le fichier d'export des releves
 DATAPHYTO = read.csv(choose.files())
 
 #Decroiser le tableau
-MELT_DATA <- melt(DATAPHYTO, id.vars = c("CD_NOM","NOM_VALIDE","RELEVE"), measure.vars = c("ARBO", "ARBU", "HERB", "MUCINALE"))
+MELT_DATA <- melt(DATAPHYTO, id.vars = c("CD_NOM","NomComplet","RELEVE"), measure.vars = c("ARBO", "ARBU", "HERB", "MUCINALE"))
+
 
 # Attribution d'un poid pour chaque classe phyto
-library(tidyverse)
 MELT_DATA$pondvalue = str_replace_all(MELT_DATA$value,c("5"="10","4"="8","3"="6","2"="4","1"="1","\\+"="0.5","r"="0.25","i"="0.125"))
 MELT_DATA$pondvalue = as.numeric(MELT_DATA$pondvalue)
 colnames(MELT_DATA) = c("CD_NOM","NOM_VALIDE","RELEVE","STRATE","value","pondvalue")
@@ -28,69 +29,12 @@ JOIN_DATA  = left_join(AGG_DATA,TAXREF_baseflor,by ="CD_NOM")
 
 JOIN_DATA = select(JOIN_DATA,RELEVE,STRATE,CD_NOM,NOM_VALIDE,Nom.scientifique, value,pondvalue,code_CATMINAT)
 
-# On retirer les espèces sans code CATMINAT
-JOIN_DATA_CALL = JOIN_DATA[!is.na(JOIN_DATA$code_CATMINAT),]
 
-DATARELEVE = data.frame(RELEVE = levels(factor(JOIN_DATA_CALL$RELEVE)),CATMINAT = "")
-#Boucle qui tourne pour chaque releve
-for(i in 1:nrow(DATARELEVE)){
-  cat("RELEVE : ",DATARELEVE$RELEVE[i],"\n")
-  JOIN_DATA_CALL = JOIN_DATA[!is.na(JOIN_DATA$code_CATMINAT),]
-  JOIN_DATA_CALL = JOIN_DATA_CALL[JOIN_DATA_CALL$RELEVE == DATARELEVE$RELEVE[i],]
-  CATMINAT_SEP = separate(JOIN_DATA_CALL, code_CATMINAT, into = c("code1", "code2", "code3", "code4", "code5","code6","code7"), sep = "/|\\.")
-  JOIN_DATA_CALL = left_join(CATMINAT_SEP,JOIN_DATA_CALL,by="CD_NOM",keep = FALSE)[,-c(13:16)]
-  # On analyse les code CATMINAT dans l'ordre d'importance
-  for(j in 1:5){
-    cat("___VERIFICATION CODE : ",j,"\n")
-    col = paste0("code",j)
-    if(all(is.na(JOIN_DATA_CALL[col]))){break}
-    AGG = aggregate(pondvalue.x~get(paste0("code",j)),data=JOIN_DATA_CALL,sum)
-    sort_value = sort(AGG$pondvalue.x,decreasing = T)
-    if(sort_value[1]<sort_value[2]*1.2 & length(sort_value)>1){
-      cat("______PROXIMITE OU EGALITE DES VALEURS AU NIVEAU",j, "\n")
-      break}
-    
-    #Attribution du code CATMINAT selon l'avancement
-    CATMIN = as.character(AGG[AGG$pondvalue.x==max(AGG$pondvalue.x),][1])
-    if(j == 1){
-      sep1 = "/" ; sep2 = ""
-    } else if(j == 2){
-      sep1 = "." ; sep2 = ""
-      } else if(j == 3){
-        sep1 = "" ; sep2 = ""
-      } else{
-          sep1 = "" ; sep2 = "."
-          }
-    DATARELEVE$CATMINAT[i] = paste0(DATARELEVE$CATMINAT[i],sep2,CATMIN,sep1)
-    JOIN_DATA_CALL = JOIN_DATA_CALL[JOIN_DATA_CALL[col]== CATMIN,]
-    
-  }
-}
+# ANALYSE PAR STRATE !
+source("R_script/Pluristrate.R")
 
-# Une fois un code attribu? ? chaque relev?, on y joint les informations de baseveg
-
-baseflor <- read.csv("baseflor.csv",sep=";",fileEncoding = "latin1")
-DATARELEVE_JOIN = inner_join(DATARELEVE,baseflor,by=c("CATMINAT"="code_CATMINAT"))
-DATARELEVE_JOIN = aggregate(DATARELEVE_JOIN,list(DATARELEVE_JOIN$RELEVE),unique)
-
-# Ajout des nomenclature de phytosociologie a JOIN_DATA
-JOIN_DATA$IDKEY = paste0(JOIN_DATA$STRATE,JOIN_DATA$CD_NOM,JOIN_DATA$RELEVE)
-JOIN_DATA_EXP = inner_join(JOIN_DATA,baseflor,by=c("code_CATMINAT"="code_CATMINAT"))
-JOIN_DATA_EXP = aggregate(JOIN_DATA_EXP,list(JOIN_DATA_EXP$IDKEY),unique)
-JOIN_DATA_EXP = select(JOIN_DATA_EXP,RELEVE,CD_NOM,NOM_VALIDE,Nom.scientifique, value,pondvalue,code_CATMINAT,CARACTERISATION_ECOLOGIQUE_.HABITAT_OPTIMAL.,INDICATION_PHYTOSOCIOLOGIQUE_CARACTERISTIQUE)
-# On retire les synonymes
-# DATARELEVE_JOIN = DATARELEVE_JOIN[!DATARELEVE_JOIN$NIVEAU %in% c("syn =","syn = pp","syn compl inval pp","syn compl pp","syn pp"),]
-
-# On retirer les colonnes inutiles
-DATARELEVE_JOIN = DATARELEVE_JOIN %>% select(RELEVE,CATMINAT,CARACTERISATION_ECOLOGIQUE_.HABITAT_OPTIMAL.,INDICATION_PHYTOSOCIOLOGIQUE_CARACTERISTIQUE)
-
-# Enregistrement des résultats
-dir.create("OUTPUT")
-write.csv(DATARELEVE_JOIN,"OUTPUT/BILAN_RELEVE.csv",fileEncoding = "UTF-8",row.names = F)
-
-# Export de JOIN_DATA
-JOIN_DATA_EXP = JOIN_DATA_EXP %>% arrange(RELEVE ,code_CATMINAT)
-write.csv(JOIN_DATA_EXP,"OUTPUT/BILAN_ESP.csv",fileEncoding = "UTF-8",row.names = F)
+# ANALYSE TOUTES STRATES CONFONDUES
+source("R_script/Monostrate.R")
 
 ################################################################################
 ###################CREATION DU FICHIER RMARKDOWN################################
