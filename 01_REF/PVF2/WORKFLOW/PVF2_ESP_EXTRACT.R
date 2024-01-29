0d548da3a4169ae40f6f9b3398ac9bb2c3127e09
Augustin Soulard <augustinsoulard@yahoo.fr>
2024-01-29 19:54:30 +0100

    v0.7.0

--- 01_REF/PVF2/WORKFLOW/PVF2_ESP_EXTRACT.R
+++ 01_REF/PVF2/WORKFLOW/PVF2_ESP_EXTRACT.R
@@ -8,13 +8,51 @@ if (!require("xlsx")) {install.packages("xlsx")}+library("xlsx")
 if (!require("stringr")) {install.packages("stringr")}+library("stringr")
 
 
+###############Creation TYPO_PVF2_70_corrige
+TYPO_PVF2_70_corrige <- read_excel("D:/Github/HABITATS/01_REF/PVF2/TYPO_PVF2_70_corrige.xlsx", 
+                                   sheet = "TYPO_PVF2_70")
 
+#Inition la typologie corrige
+TYPO_PVF2_70_corrige$COMBINAISON_ESPECES_CORR = NA
+
+#Boucle de correction
+for(j in 1:nrow(TYPO_PVF2_70_corrige)){
+  cat("Ligne : ",j,"\n")
+  if(is.na(TYPO_PVF2_70_corrige$COMBINAISON_ESPECES[j])){
+    next
+  }
+  mots = strsplit(TYPO_PVF2_70_corrige$COMBINAISON_ESPECES[j], " |,|\n")[[1]]
+  
+  lastGenre = "ERREUR"
+  lastAdjectif = "ERREUR"
+  
+  for (i in 1:length(mots)) {
+    #Traitement des genres
+    if(str_detect(mots[i],"^[A-Z]\\.$") == TRUE){
+      mots[i] = lastGenre
+    }
+    if(str_detect(mots[i],"^[A-Z]") == TRUE){
+      lastGenre = mots[i]
+    }
+    #Traitement des infrataxons
+    if(str_detect(mots[i],"\\.$") == FALSE){
+      lastAdjectif = mots[i]
+    }
+    if(str_detect(mots[i],"^[a-z]\\.$") == TRUE){
+      mots[i] = lastAdjectif
+    }
+  }
+  TYPO_PVF2_70_corrige$COMBINAISON_ESPECES_CORR[j] = paste(mots,collapse = " ")
+}
+# Enregistrement en XLSX
+write.csv(TYPO_PVF2_70_corrige,"TYPO_PVF2_70_corrige.csv",fileEncoding = "UTF-8",row.names = F)
 
 # Chargement des donnees
 
 #PVF2
 
-TYPO_PVF2_70 <- read_excel("../TYPO_PVF2_70_corrige.xlsx", sheet = "TYPO_PVF2_70")
+#TYPO_PVF2_70 <- read_excel("../TYPO_PVF2_70_corrige.xlsx", sheet = "TYPO_PVF2_70")
+TYPO_PVF2_70 = TYPO_PVF2_70_corrige
 
 #TAXREFv16
 TAXREFv16_FLORE_FR = read.csv("TAXREFv16_FLORE_FR.csv",fileEncoding = "UTF-8")
@@ -28,13 +66,13 @@ TAB_ESP_PVF2 = data.frame(CD_HAB = character(0),LB_CODE= character(0),LB_HAB_FR=
 
 # Boucle de rattachement TAXREF - PVF2 avec affichage
 for(i in 1:nrow(TYPO_PVF2_70)){
-  resultats <- sapply(TAXREFv16_FLORE_FR$LB_NOM, function(x) grepl(x,TYPO_PVF2_70$COMBINAISON_ESPECES[i]))
+  resultats <- sapply(TAXREFv16_FLORE_FR$LB_NOM, function(x) grepl(x,TYPO_PVF2_70$COMBINAISON_ESPECES_CORR[i]))
   TAXREFv16_FLORE_FR[resultats,]$LB_NOM
   
     cat("PVF2 ligne : ",i,"\n")
     if(nrow(data.frame(TAXREFv16_FLORE_FR[resultats,]$LB_NOM))>0){
       
-      TAB_TEMP = data.frame(CD_HAB = TYPO_PVF2_70$CD_HAB[i],LB_CODE= TYPO_PVF2_70$LB_CODE[i],LB_HAB_FR=TYPO_PVF2_70$LB_HAB_FR[i],CD_NOM=TAXREFv16_FLORE_FR[resultats,]$LB_NOM,LB_NOM=TAXREFv16_FLORE_FR[resultats,]$CD_REF)
+      TAB_TEMP = data.frame(CD_HAB = TYPO_PVF2_70$CD_HAB[i],LB_CODE= TYPO_PVF2_70$LB_CODE[i],LB_HAB_FR=TYPO_PVF2_70$LB_HAB_FR[i],CD_NOM=TAXREFv16_FLORE_FR[resultats,]$CD_REF,LB_NOM=TAXREFv16_FLORE_FR[resultats,]$LB_NOM)
       
       TAB_ESP_PVF2 = rbind(TAB_ESP_PVF2,TAB_TEMP)
  
@@ -42,78 +80,7 @@ for(i in 1:nrow(TYPO_PVF2_70)){
 }
 
 # Enregistrement en XLSX
-write.xlsx(TAB_ESP_PVF2,"TAB_ESP_PVF2.xlsx")
-
-
-########WORKFLOW####################
-
-any(str_detect(TAXREFv16_FLORE_FR$LB_NOM, TYPO_PVF2_70$COMBINAISON_ESPECES[14]))
+write.csv(TAB_ESP_PVF2,"TAB_ESP_PVF2.csv",fileEncoding = "UTF-8",row.names = F)
 
 
 
-##############TEST EXPRESSION REGULIERE
-
-
-# Fonction pour détecter et corriger les abréviations de genre
-
-phrase = "Nardus stricta, Achillea ptarmica subsp. pyrenaica, Agrostis rupestris subsp. rupestris, 
-Carex umbrosa subsp. huetiana, Epikeros pyrenaeus (= Selinum pyrenaeum), Euphrasia minima subsp. m., 
-Gentiana alpina, G. pyrenaica, Hieracium lactucella, Pedicularis pyrenaica, Ranunculus pyrenaeus, Trifolium alpinum, 
-Selaginella selaginoides"
-
-
-mots = strsplit(phrase, " |,|\n")[[1]]
-
-test_vide <- function(x) {
-  return(nchar(x) != 0)
-}
-
-mot_Genre = mots[str_detect(mots,"^[A-Z]")]
-
-
-#Liste de mot dans ma phrase
-mots = mots[unlist(lapply(mot, function(x) test_vide(x)))]
-
-grepl( mots[6])
-
-sub("^([A-Z]+)\\.$", "\\1", mots[6])
-
-gsub("^([A-Z]+)\\s+.*", "\\1", mots[6])
-
-# Fonction pour détecter et corriger les abréviations de genre dans une phrase
-detecter_et_corriger_abreviation_phrase <- function(phrase) {
-  mots <- strsplit(phrase, " ")[[1]]
-  genre_precedent <- NULL
-  
-  for (i in seq_along(mots)) {
-    mot <- mots[i]
-    
-    # Utilisation d'une expression régulière pour détecter les abréviations de genre
-    if (grepl("^\\w+\\.$", mot)) {
-      # Extraire l'abréviation de genre
-      abrev_genre <- gsub("^([A-Z]+)\\.$", "\\1", mot)
-      
-      # Corriger l'abréviation en utilisant le genre précédent
-      if (!is.null(genre_precedent)) {
-        mots[i] <- paste(genre_precedent, gsub("^[A-Z]+\\.$", "", mot), sep = " ")
-      }
-    } else {
-      # Mettre à jour le genre précédent si le mot n'est pas une abréviation
-      genre_precedent <- gsub("^([A-Z]+)\\s+.*", "\\1", mot)
-    }
-  }
-  
-  return(paste(mots, collapse = " "))
-}
-
-# Liste de phrases avec des noms d'espèces
-phrases <- c("Nardus stricta est une plante.", 
-             "Achillea ptarmica subsp. pyrenaica est une autre espèce.",
-             "Gentiana alpina, G. pyrenaica, et Hieracium lactucella sont des exemples.")
-
-# Appliquer la fonction sur la liste de phrases
-phrases_corrigees <- sapply(phrases, detecter_et_corriger_abreviation_phrase)
-
-# Afficher les résultats
-cat("Liste de phrases initiale:\n", paste(phrases, collapse = "\n"), "\n\n")
-cat("Liste de phrases corrigée:\n", paste(phrases_corrigees, collapse = "\n"))
